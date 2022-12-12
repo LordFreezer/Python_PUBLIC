125fdef127379210d87c7728ae504a5da4ace499
Chad Marshall <34351071+LordFreezer@users.noreply.github.com>
2022-12-12 01:13:09 -0600

    Update sum_of_two_num_in_arr.py

--- sum_of_two_num_in_arr.py
+++ sum_of_two_num_in_arr.py
@@ -1,10 +1,19 @@
-def twoSum(arr, sum):
+def twoSum1(arr, sum):
     for i in range(len(arr)):
         for j in range(len(arr) - i - 1):
             if(arr[i] + arr[j] == sum):
-                print(str(arr[i]) + " " + str(arr[j]))
-                return i, j
+#                print(str(arr[i]) + " " + str(arr[j]))
+                return arr[i], arr[j]
+                
+def twoSum2(arr, sum):
+    dict = {}
+    for i in range(len(arr)):
+        dict[sum - arr[i]] = arr[i]
+        try:
+            return arr[i], dict[arr[i]]
+        except KeyError:
+            dict[sum - arr[i]] = arr[i]
 
 arr = [1, 3, 5, 6, 11, 23]
 sum = 9
-print("Indices: "+str(twoSum(arr, sum)))
+print(twoSum2(arr, sum))
